ebf3115ede32eaae107620963258188e26a22f93
mest-yamaru <83887766+mest-yamaru@users.noreply.github.com>
2022-09-20 03:24:22 +0900

    andwhere文

--- src/Repository/PersonRepository.php
+++ src/Repository/PersonRepository.php
@@ -51,9 +51,12 @@ class PersonRepository extends ServiceEntityRepository
 
     public function findByAge($value)
     {
-        return $this->createQueryBuilder('p')
-            ->where('p.age >= ?1')
-            ->setParameter(1, $value)
+        $arr = explode(',', $value);
+        $builder = $this->createQueryBuilder('p');
+        return $builder
+            ->where($builder->expr()->gte('p.age', '?1'))
+            ->andwhere($builder->expr()->lte('p.age', '?2'))
+            ->setParameters(array(1 => $arr[0], 2 => $arr[1]))
             ->getQuery()
             ->getResult();
     }
