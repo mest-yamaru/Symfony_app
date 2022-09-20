1a8150a7c57b8431a579847274c389fdd36f534f
mest-yamaru <83887766+mest-yamaru@users.noreply.github.com>
2022-09-21 00:11:30 +0900

    messageコントローラーの作成

--- src/Controller/MessageController.php
+++ src/Controller/MessageController.php
@@ -2,9 +2,23 @@
 
 namespace App\Controller;
 
+use APP\Entity\Person;
+use App\Entity\Message;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\Extension\Core\Type\IntegerType;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+
+use App\Form\PersonType;
+use App\Form\MessageType;
+use Doctrine\ORM\Query\ResultSetMappingBuilder;
+use Symfony\Component\Validator\Validator\ValidatorInterface;
+
+use Symfony\Component\Validator\Constraints as Assert;
 
 class MessageController extends AbstractController
 {
@@ -13,8 +27,41 @@ class MessageController extends AbstractController
      */
     public function index(): Response
     {
+        $repository = $this->getDoctrine()->getRepository(Message::class);
+        $data = $repository->findAll();
         return $this->render('message/index.html.twig', [
-            'controller_name' => 'MessageController',
+            'title' => 'Message',
+            'data' => $data,
+        ]);
+    }
+
+    /**
+     * @Route("/message/cerate", name="message/create")
+     */
+    public function create(Request $request, ValidatorInterface $validator)
+    {
+        $message = new Message();
+        $form = $this->createForm(MessageType::class, $message);
+        $form->handleRequest($request);
+
+        if ($request->getMethod() == 'POST') {
+            $message = $form->getData();
+            $errors = $validator->validate($message);
+            if(count($errors) == 0) {
+                $manager = $this->getDoctrine()->getManager();
+                $manager->persist($message);
+                $manager->flush();
+                return $this->redirect('/message');
+            } else {
+                $msg = "oh...can't posted...";
+            }
+        } else {
+            $msg = "type your message!";
+        }
+        return $this->render('message/create.html.twig', [
+            'title' => 'Hello',
+            'message' => $msg,
+            'form' => $form->createView(),
         ]);
     }
 }
