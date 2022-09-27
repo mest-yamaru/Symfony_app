dcc489d2a1cdf7f17ef64a9af2a950c7e23dcaa8
mest-yamaru <83887766+mest-yamaru@users.noreply.github.com>
2022-09-28 07:55:47 +0900

    タイポ修正

--- src/Controller/SecurityController.php
+++ src/Controller/SecurityController.php
@@ -3,7 +3,7 @@ namespace App\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\Routing\Annotation\Route;
-use Symfony\Component\Security\Http\Authentication\Authenticationutils;
+use Symfony\Component\Security\Http\Authentication\AuthenticationUtils;
 
 class SecurityController extends AbstractController
 {
